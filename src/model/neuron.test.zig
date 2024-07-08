41a5991629c3873fc953c0b5121fa140c4bc604f
Mikolaj Witkowski <mikolaj@NTB53.etops-ag.local>
2024-07-08 11:44:06 +0200

    refactor: neuron rng

--- src/model/neuron.test.zig
+++ src/model/neuron.test.zig
@@ -13,9 +13,12 @@ test "create Neuron" {
 
 test "activate Neuron" {
     var _neuron = Neuron.create(2, testAllocator);
-    const input = [2]f32{ 0.0, 1.0 };
+    var input = std.ArrayList(f32).init(testAllocator);
+    try input.append(0.0);
+    try input.append(1.1);
     try _neuron.activateInput(input);
     const w2 = _neuron.weights.items[1].value;
-    try expectEqual(_neuron.output.items[1].value, (w2 * input[1]) + _neuron.bias.value);
+    try expectEqual(_neuron.output.items[1].value, (w2 * input.items[1]) + _neuron.bias.value);
     try _neuron.freeMemory();
+    input.deinit();
 }
--- src/model/neuron.zig
+++ src/model/neuron.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const value = @import("value.zig");
 const Value = value.Value;
 const Allocator = std.mem.Allocator;
+var randomGenerator = std.rand.DefaultPrng.init(42);
 
 pub const Neuron = struct {
     bias: Value,
@@ -17,13 +18,14 @@ pub const Neuron = struct {
     pub fn create(inputShape: usize, allocator: std.mem.Allocator) Neuron {
         var weights = std.ArrayList(Value).init(allocator);
         const output = std.ArrayList(Value).init(allocator);
+        const rng = randomGenerator.random();
 
         for (0..inputShape) |_| {
-            const randomFloat = std.crypto.random.float(f32);
+            const randomFloat = rng.float(f32);
             const newValue = Value.create(randomFloat, allocator);
             weights.append(newValue) catch {};
         }
-        const randomizedBias = std.crypto.random.float(f32);
+        const randomizedBias = rng.float(f32);
         const newNeuron = Neuron{
             .weights = weights,
             .bias = Value.create(randomizedBias, allocator),
@@ -33,8 +35,8 @@ pub const Neuron = struct {
         return newNeuron;
     }
 
-    pub fn activateInput(self: *Neuron, input: [2]f32) !void {
-        for (input, 0..) |element, elementIndex| {
+    pub fn activateInput(self: *Neuron, input: std.ArrayList(f32)) !void {
+        for (input.items, 0..) |element, elementIndex| {
             const newOutput = Value.create(element * self.weights.items[elementIndex].value + self.bias.value, self.allocator);
             try self.output.append(newOutput);
         }
